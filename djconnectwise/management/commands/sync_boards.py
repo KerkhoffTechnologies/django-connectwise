599140107afaf3b8a5c599014bc1256c02feaf37
Joel Legris <joel@aparat-tech.com>
2017-02-10 08:57:45 -0800

    #162 - Implemented sync boards command

--- djconnectwise/management/commands/sync_boards.py
+++ djconnectwise/management/commands/sync_boards.py
@@ -1,10 +1,13 @@
 from django.core.management.base import BaseCommand
-from djconnectwise.api import ServiceAPIClient
+from djconnectwise.sync import BoardSynchronizer
 
 
 class Command(BaseCommand):
-    help = 'Synchronize local board and status data with connectwise server'
+    help = 'Synchronize local board data with connectwise server'
 
     def handle(self, *args, **options):
-        client = ServiceAPIClient()
-        print(client.get_boards())
+        synchronizer = BoardSynchronizer()
+
+        created_count, updated_count = synchronizer.sync()
+        msg = 'Synced Boards - Created: {} , Updated: {}'
+        self.stdout.write(msg.format(created_count, updated_count))
