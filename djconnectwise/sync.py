c725af020fd8546225f6be1d8e39b0871aa973ee
Curtis Hildebrand <trinitonesounds@users.noreply.github.com>
2017-07-31 14:35:11 -0700

    Remove unneeded spaces. Unit test lastUpdated condition

--- djconnectwise/sync.py
+++ djconnectwise/sync.py
@@ -639,7 +639,7 @@ class TicketSynchronizer(Synchronizer):
         if sync_job_qset.exists() and not reset:
             last_sync_job_time = sync_job_qset.last().start_time.isoformat()
             self.api_conditions.append(
-                "lastUpdated > [{0}]".format(last_sync_job_time)
+                "lastUpdated>[{0}]".format(last_sync_job_time)
             )
 
         return super().sync(reset=reset)
