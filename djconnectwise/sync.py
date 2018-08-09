659ef4097b1a1390f322b6ef7db5afff784ff5cd
swolfe <samuel.wolfe@student.ufv.ca>
2018-08-09 09:57:32 -0700

    Added calendar model

--- djconnectwise/sync.py
+++ djconnectwise/sync.py
@@ -1359,6 +1359,10 @@ class SLASynchronizer(Synchronizer):
         instance.plan_within = json_data['planWithin']
         instance.resolution_hours = json_data['resolutionHours']
 
+        instance.based_on = json_data.get('basedOn')
+        instance.calendar = json_data.get('customCalendar')
+
+
     def get_page(self, *args, **kwargs):
         return self.client.get_slas(*args, **kwargs)
 
