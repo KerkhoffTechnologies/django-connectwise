62f0381cc273b99c675ff27f27485521202dbec5
Cameron Sweeney <cp_sweeney@hotmail.com>
2018-08-22 12:17:42 -0700

    Increase max digits

--- djconnectwise/models.py
+++ djconnectwise/models.py
@@ -445,11 +445,11 @@ class TimeEntry(models.Model):
         return str(self.id) or ''
 
     actual_hours = models.DecimalField(
-        blank=True, null=True, decimal_places=2, max_digits=6)
+        blank=True, null=True, decimal_places=2, max_digits=9)
     billable_option = models.CharField(choices=BILL_TYPES, max_length=250)
     charge_to_type = models.CharField(choices=CHARGE_TYPES, max_length=250)
     hours_deduct = models.DecimalField(
-        blank=True, null=True, decimal_places=2, max_digits=6)
+        blank=True, null=True, decimal_places=2, max_digits=9)
     internal_notes = models.TextField(blank=True, null=True, max_length=2000)
     notes = models.TextField(blank=True, null=True, max_length=2000)
     time_start = models.DateTimeField(blank=True, null=True)
@@ -568,11 +568,11 @@ class AvailableProjectManager(models.Manager):
 class Project(TimeStampedModel):
     name = models.CharField(max_length=200)
     actual_hours = models.DecimalField(
-        blank=True, null=True, decimal_places=2, max_digits=6)
+        blank=True, null=True, decimal_places=2, max_digits=9)
     budget_hours = models.DecimalField(
-        blank=True, null=True, decimal_places=2, max_digits=6)
+        blank=True, null=True, decimal_places=2, max_digits=9)
     scheduled_hours = models.DecimalField(
-        blank=True, null=True, decimal_places=2, max_digits=6)
+        blank=True, null=True, decimal_places=2, max_digits=9)
 
     status = models.ForeignKey(
         'ProjectStatus', blank=True, null=True, on_delete=models.SET_NULL)
@@ -743,11 +743,11 @@ class Ticket(TimeStampedModel):
     )
 
     actual_hours = models.DecimalField(
-        blank=True, null=True, decimal_places=2, max_digits=6)
+        blank=True, null=True, decimal_places=2, max_digits=9)
     agreement_id = models.IntegerField(blank=True, null=True)
     approved = models.NullBooleanField(blank=True, null=True)
     budget_hours = models.DecimalField(
-        blank=True, null=True, decimal_places=2, max_digits=6)
+        blank=True, null=True, decimal_places=2, max_digits=9)
     closed_by = models.CharField(blank=True, null=True, max_length=250)
     closed_date_utc = models.DateTimeField(blank=True, null=True)
     closed_flag = models.NullBooleanField(blank=True, null=True)
