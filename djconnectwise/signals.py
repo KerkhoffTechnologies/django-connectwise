0c2ec4a7652b7f54062dc916de333f5dca89f9d0
swolfe <samuel.wolfe@student.ufv.ca>
2018-10-05 13:36:30 -0700

    Modified signals.py

--- djconnectwise/signals.py
+++ djconnectwise/signals.py
@@ -13,7 +13,12 @@ def handle_ticket_sla_update_pre_save(sender, instance, **kwargs):
     # Signal for updating a tickets SLA information if necessary
     try:
         old_ticket = Ticket.objects.get(id=instance.id)
-        if old_ticket.status > instance.status or \
+        if not old_ticket.status.escalation_status or \
+                not instance.status.escalation_status:
+            # If there are unit tests running that don't require SLA data
+            # or if the instance does not have SLA data, return
+            return
+        elif old_ticket.status > instance.status or \
                 old_ticket.status < instance.status:
             instance.calculate_sla_expiry(
                     old_status=old_ticket.status)
@@ -22,9 +27,3 @@ def handle_ticket_sla_update_pre_save(sender, instance, **kwargs):
     except Ticket.DoesNotExist:
         # This is normal when creating a new ticket.
         instance.calculate_sla_expiry()
-    except TypeError:
-        # This will happen during unit tests unrelated to SLAs
-        # Rather than updating all the old unit tests with quite a bit of
-        # SLA data they do not need at the moment, just skip SLA calculations
-        # on those tests
-        pass
