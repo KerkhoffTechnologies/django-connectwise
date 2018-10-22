b36d25d490e54a50de097669d9cb4e46ba0e364d
Cameron Sweeney <cameron@kerkhofftech.ca>
2018-10-22 11:50:03 -0700

    Fix extra punctuation

--- djconnectwise/api.py
+++ djconnectwise/api.py
@@ -189,14 +189,19 @@ class ConnectWiseAPIClient(object):
         # error = error.args[0].decode("utf-8")
         error = error.replace('\r\n', '')
         messages = []
+
         try:
             error = json.loads(error)
+            primary_error_msg = '{}.'.format(error.get('message'))
+
             if error.get('errors'):
                 for error_message in error.get('errors'):
-                    messages.append(error_message.get('message'))
+                    messages.append('{}.'.format(error_message.get('message')))
 
-            msg = '{}; {}.'.format(error.get('message'),
-                                   '. The error was: '.join(messages))
+            messages = ' The error was: '.join(messages)
+
+            msg = '{} {}'.format(primary_error_msg, messages)
+
         except json.decoder.JSONDecodeError:
             # JSON decoding failed
             msg = 'An error occurred: {} {}'.format(response.status_code,
