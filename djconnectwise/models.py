2aa994a806eb06904034ad581a5a41471a3189e1
swolfe <samuel.wolfe@student.ufv.ca>
2020-05-15 17:45:42 -0700

    changed str method of type

--- djconnectwise/models.py
+++ djconnectwise/models.py
@@ -1801,7 +1801,7 @@ class Type(TimeStampedModel):
         verbose_name_plural = 'Types'
 
     def __str__(self):
-        return self.name
+        return "{} - {}".format(self.name, self.board)
 
 
 class SubType(TimeStampedModel):
