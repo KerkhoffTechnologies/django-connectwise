55fff6972bda0a5cab660dc564fad7549c04091a
Zeryab Khan <zeryabkhan91@gmail.com>
2026-01-14 18:12:16 +0500

    add predecessor fields

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 LONG_DESCRIPTION = open('README.md').read()
 
-VERSION = (1, 13, 7)
+VERSION = (1, 13, 8)
 
 project_version = '.'.join(map(str, VERSION))
 
